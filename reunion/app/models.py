07f68582e3dd9855bba9081c15739833cd1ac9bd
mayurkulkarni <mayur.kulkarni@bajajfinserv.in>
2022-10-02 15:21:45 +0530

    Changes done for email authentication

--- reunion/app/models.py
+++ reunion/app/models.py
@@ -1,14 +1,15 @@
-from unittest.util import _MAX_LENGTH
 from django.db import models
-from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import AbstractUser
-    
+
 class Profile(AbstractUser):
+    email = models.EmailField(unique=True,blank=False,null=False)
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField( max_length=30, blank=True)
     following = ArrayField(ArrayField(models.IntegerField()),blank=True, null=True)
     followers = ArrayField(ArrayField(models.IntegerField()),blank=True, null=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 class Post(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
